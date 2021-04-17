25be4a61608ce647d1e7453728cdf1a9b752d658
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-04-17 22:23:58 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
-const fs = require('fs-extra');
 require('dotenv').config()
 const app = express()
 app.use(express.static('doctors'));
@@ -12,7 +11,7 @@ app.use(fileUpload({
 }))
 app.use(cors());
 app.use(bodyParser.json())
-const port = process.PORT || 5000
+const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
 const { static } = require('express');
 console.log(process.env.DB_PASS, process.env.DB_USER)
@@ -82,8 +81,6 @@ client.connect(err => {
   })
 
 });
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
